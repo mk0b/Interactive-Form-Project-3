28fc9db31e411b1ff50d53c863c7b64f6d3776e8
Megan Katherine O'Brien <megankob@gmail.com>
2019-10-25 19:12:47 -0400

    Initial Commit
    Created the script.js file and downloaded the jQuery file and added both to the project/html file.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* Interactive Form Project 3 - TeamTreehouse Full Stack JavaScript Techdegree.
+Megan Katherine O'Brien's List Pagination and Filtering Project 3 
+as a TeamTreehouse Full Stack JavaScript Talent Path student.*/
+
+// Project 3 Guide - https://drive.google.com/file/d/1Vw658-9KUiUZ5yHaABvkytC9W2QBYiW_/view?usp=sharing
