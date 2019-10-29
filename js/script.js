f57579fcb17fa09d288b479a3c9be72771300900
Megan Katherine O'Brien <megankob@gmail.com>
2019-10-29 17:21:17 -0400

    Update
    Added some HTML for the hidden Other section in Job Role.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ Megan Katherine O'Brien's List Pagination and Filtering Project 3
 as a TeamTreehouse Full Stack JavaScript Talent Path student.*/
 
 // Project 3 Guide - https://drive.google.com/file/d/1Vw658-9KUiUZ5yHaABvkytC9W2QBYiW_/view?usp=sharing
+
+//TODO: Use the CDN way of getting the jquery.
+//TODO: Fill out the read me.
+//TODO: Clean up comments
+//TODO: Refactor Code
+//TODO: css/reset.css or css/normalize.css - Google and add to project (Opt)
+//TODO: Add placeholder text to make more usable?
+
+
+//This sets the curser to the first input on page load.
+window.onload = () => {
+    $('#name').focus();
+}
+
+//added this code to the html file: <input type="text" id="other-title" name="user_other_jobtitle" placeholder="Your Job Role"></input>
+
+$('#other-title').hide();
+
+
+
+//Event Handlers
+
+
