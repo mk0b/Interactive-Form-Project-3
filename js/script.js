1de244262759d2db4a3468f1e6f97b51b061f0bc
Megan Katherine O'Brien <megankob@gmail.com>
2019-11-04 17:07:48 -0500

    Update script.js
    Updates

--- js/script.js
+++ js/script.js
@@ -65,20 +65,8 @@ $('#payment option[value="Credit Card"]').prop('selected', true);
 //Disabling "Select payment Method" in the payment info dropdown so they can't choose it from the list.
 $('#payment option[value="select method"]').prop('disabled', true);
 
-//added this code to the html file: <input type="text" id="other-title" name="user_other_jobtitle" placeholder="Your Job Role"></input>
+//Note: Added this code to the html file: <input type="text" id="other-title" name="user_other_jobtitle" placeholder="Your Job Role"></input>
 //so that it still shows up when javascript is turned off in the browser.
-
-//validation Requirements//
-
-/* 
-- Name field can't be blank.
-- Email field must be a validly formatted e-mail address (you don't have to check that it's a real e-mail address, just that it's formatted like one: dave@teamtreehouse.com for example.
-- User must select at least one checkbox under the "Register for Activities" section of the form.
-- If the selected payment option is "Credit Card," make sure the user has supplied a Credit Card number, a Zip Code, and a 3 number CVV value before the form can be submitted.
-- Credit Card field should only accept a number between 13 and 16 digits.
-- The Zip Code field should accept a 5-digit number.
-- The CVV should only accept a number that is exactly 3 digits long.
-*/
 
 //Validation Functions//
 
@@ -276,19 +264,17 @@ $paymentDropDown.change( (event) => {
 
 });
 
-//real time listener for email field not being correct email format
-$emailField.on('input', () => {
-    if (isValidEmail($emailField)) {
-        $emailBlankHelperText.show();
-        $emailField.toggleClass('input-border-red');
+//Real time listener for email field not being correct email format.
+$emailField.on('input', (event) => {
+    const text = event.target.value;
+    if (isValidEmail(text)) {
+        $emailHelperText.hide();
     } else {
-        $emailBlankHelperText.hide();
-        $emailField.toggleClass('input-border-red');
+        $emailHelperText.show();
     }
-
 });
 
-//submit event handler if statements for validation messages.
+//Submit event handler if statements for validation messages.
 
 $form.on('submit', (event) => {
 
